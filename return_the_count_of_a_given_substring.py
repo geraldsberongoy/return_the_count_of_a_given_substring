62a23782ef8c33a8a97e93ea06a6ab31a9c0aaf5
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-22 00:37:12 +0800

    Pseudocode

--- return_the_count_of_a_given_substring.py
+++ return_the_count_of_a_given_substring.py
@@ -1,2 +1,7 @@
 # Exercise 7: Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
+
+# Pseudocode
+# assignment variable for a string
+# using a count()
+# print
